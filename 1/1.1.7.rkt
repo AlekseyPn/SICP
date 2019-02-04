7b6639a74e54d12163ef7d85c75d45d3ff689ab0
Aleksey Peresmekhin <aleksfers@icloud.com>
2019-02-04 20:53:04 +0500

    better sqrt

--- 1/1.1.7.rkt
+++ 1/1.1.7.rkt
@@ -1,5 +1,21 @@
 (module sicp racket
     ;; 1.7 Улучшенный алгоритм для good-enough?
-    (define (good-enough? guess prev-guess)
-        (< (abs (/ (- guess prev-guess) prev-guess)) 0.001))
+    (define (sqrt x)
+        (define (good-enough? guess prev-guess)
+            (< (abs (/ (- guess prev-guess) prev-guess)) 0.001))
+
+        (define (average x y)
+            (/ (+ x y) 2))
+
+        (define (improve guess)
+            (average guess (/ x guess)))
+
+        (define (sqrt-iter guess prev-guess)
+            (if (good-enough? guess prev-guess)
+                guess
+                (sqrt-iter (improve guess)
+                        guess)))
+    
+        (sqrt-iter 1.0 0.5))
+    (sqrt 10)
 )
